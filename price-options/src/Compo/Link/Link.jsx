01ce3c3144d507676fd51a7194e11a407b27e937
Shoeb1989 <psikder09@gmail.com>
2024-08-29 22:56:58 +0600

    new compo

--- price-options/src/Compo/Link/Link.jsx
+++ price-options/src/Compo/Link/Link.jsx
@@ -1,18 +1,15 @@
-import React from 'react';
-import PropTypes from 'prop-types'; // ES6
+import PropTypes from 'prop-types'; 
 
 const Link = ({route}) => {
     return (
-        <li className='mr-10 px-6 hover:bg-yellow-200' key={route.id}>
-            
-            <a href={`route.path`}> {route.name} </a> 
-            
+        <li className="mr-10 px-6 hover:bg-yellow-500">
+            <a href={route.path}>{route.name}</a>
         </li>
     );
 };
 
-Link.prototype = {
-    route:PropTypes.object
+Link.propTypes = {
+    route: PropTypes.object
 }
 
 export default Link;
--- price-options/src/Compo/PriceOptions/PriceOption/PriceOption.jsx
+++ price-options/src/Compo/PriceOptions/PriceOption/PriceOption.jsx
@@ -1,19 +1,28 @@
 import PropTypes from 'prop-types';
+import Feature from '../../Feature/Feature';
 
 const PriceOption = ({ option }) => {
     const { name, price, features } = option;
 
 
     return (
-        <div>
+        <div className='bg-blue-300 rounded-md p-4 text-white'  >
             <h2>
                 <span className='text-7xl' >{price}</span>
                 <span className='text-2xl'>/mon</span>
             </h2>
+            
 
             <h4 className='text-3xl'>{name}</h4>
-            
-        </div>
+
+            <div className='pl-6 flex-grow'>
+                {
+                    features.map((feature, index) => <Feature key={index} feature={feature}></Feature>)
+                }
+            </div>
+
+            <button className='mt-12 bg-green-600 w-full py-2 font-bold rounded-lg hover:bg-green-900'>Buy Now</button>
+            </div>
     );
 };
 
