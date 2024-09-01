50e632991afaece6b04e5858fd21fa4e6f4dd347
Shoeb1989 <psikder09@gmail.com>
2024-09-01 22:06:33 +0600

    onSubmit handelar

--- React-Router/src/App.jsx
+++ React-Router/src/App.jsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import App from './App.jsx'
 import './App.css'
 
-function App () {
-
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
